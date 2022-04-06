8c1ee0a74e53dcac0d0111e95898ea9be004a518
tpal-dev <tpal.developer@gmail.com>
2022-04-06 13:10:56 +0200

    add use case unit test

---
+++ test/features/sample/domain/usecases/get_post_usecase_test.dart
@@ -0,0 +1,51 @@
+import 'package:http_client/core/either/either.dart';
+import 'package:http_client/core/error/failure.dart';
+import 'package:http_client/features/sample/domain/entities/post_entity.dart';
+import 'package:http_client/features/sample/domain/usecases/get_post_usecase.dart';
+import 'package:mocktail/mocktail.dart';
+import 'package:test/test.dart';
+
+import '../../../../helpers/mocked_data.dart';
+import '../../../../helpers/mocks.dart';
+
+void main() {
+  late GetPostUseCase useCase;
+  late MockPostRepository mockRepository;
+
+  setUp(() {
+    mockRepository = MockPostRepository();
+    useCase = GetPostUseCase(mockRepository);
+  });
+  group(
+    'GetPostUseCase',
+    () {
+      test(
+        'should get PostEntity form the repository',
+        () async {
+          // arrange
+          when(() => mockRepository.getPost()).thenAnswer((_) async => const Right(mockedPost));
+          // act
+          final result = await useCase();
+          // assert
+          expect(result, const Right<Failure, PostEntity>(mockedPost));
+          verify((() => mockRepository.getPost())).called(1);
+          verifyNoMoreInteractions(mockRepository);
+        },
+      );
+
+      test(
+        'should get Failure form the repository',
+        () async {
+          // arrange
+          when(() => mockRepository.getPost()).thenAnswer((_) async => const Left(mockedFailureNotFound));
+          // act
+          final result = await useCase();
+          // assert
+          expect(result, const Left<Failure, PostEntity>(mockedFailureNotFound));
+          verify((() => mockRepository.getPost())).called(1);
+          verifyNoMoreInteractions(mockRepository);
+        },
+      );
+    },
+  );
+}
--- test/helpers/mocks.dart
+++ test/helpers/mocks.dart
@@ -1,5 +1,8 @@
-// Sample Feat
+import 'package:http_client/features/sample/domain/repositories/post_repository.dart';
 import 'package:http_client/features/sample/domain/usecases/get_post_usecase.dart';
 import 'package:mocktail/mocktail.dart';
 
+// Sample Feat
 class MockGetPostUseCase extends Mock implements GetPostUseCase {}
+
+class MockPostRepository extends Mock implements PostRepository {}
